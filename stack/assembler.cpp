f52897a71b8dd33d7069be28841e324e45c4105d
Curtiss Wiggins <cxly15@gmail.com>
2012-04-08 22:31:26 -0400

    modified:   stack/Makefile 	modified:   stack/assembler.cpp 	modified:   stack/postfixcon.cpp 	modified:   stack/result.txt

--- stack/assembler.cpp
+++ stack/assembler.cpp
@@ -23,14 +23,69 @@
 #include <cstdlib>
 #include "../string/string.h"
 #include "stack.h"
-
+#include "postfixcon.h"
 ///////////////////////////////////////////////////////////////////////////////
-string
 
-int main(int argc, char **argv){
+int main(int argc, char *argv[])
+{
   
 	//Error if less than 2 commandline arguments. Means user did not
 	//specify input file.
 	if(argc < 2)
 		output_usage_and_exit(argv[0]);
+	
+	std::ifstream in(argv[1]);
+	if(!in){
+		std::cerr << "Couldn't open: " << argv[1] << std::endl;
+		exit(2);
+	}
+	
+	string temp, infix;
+	
+	if(argc==3){
+		std::ofstream out(argv[2]);
+		if(!out){
+			std::cerr << "Couldn't open: " << argv[2] << std::endl;
+			exit(2);
+		}
+		while(in>>temp){
+			
+			out << "Infix: " << temp << std::endl;
+
+			infix = temp.substr(0, temp.length()-2);
+			
+			std::vector<string> vec = infix.split(' ');
+			
+			string postfix = infix2postfix(vec);
+			
+			out << "postfix: " << postfix << std::endl;
+			std::vector<string> vec2 = postfix.split(' ');
+			out << "Assembler:" << std::endl;
+			Stack<string> assembler = postfix2assembler(vec2);
+			outputassembler(out, assembler);
+		}
+	}else{
+		
+		while(in>>temp){
+			
+		std::cout << "Infix: " << temp << std::endl;
+		
+		infix = temp.substr(0, temp.length()-2);
+		
+		std::vector<string> vec = infix.split(' ');
+		
+		string postfix = infix2postfix(vec);
+		
+		std::cout << "Postfix: " << postfix << std::endl;
+		
+		std::vector<string> vec2 = postfix.split(' ');
+		
+		std::cout << "Assembler: " << std::endl;
+		
+		Stack<string> assembler = postfix2assembler(vec2);
+		outputassembler(std::cout, assembler);
+		}
+	}
+ return 0;		
 }
+
--- stack/postfixcon.cpp
+++ stack/postfixcon.cpp
@@ -34,7 +34,7 @@ string infix2postfix(std::vector<string>& infix){
 				op  = s.pop();
 				lhs = s.pop();
 
-				s.push(lhs+"  "+rhs+"  "+op);
+				s.push(lhs+" "+rhs+" "+op);
 			}else{
 				s.push(infix[i]);
 			}
@@ -46,15 +46,14 @@ string infix2postfix(std::vector<string>& infix){
 
 
 Stack<string> postfix2assembler(std::vector<string>& p){
-	Stack<string> s, a;
+	Stack<string> s, a, as;
 
 	int i = 0, j = p.size();
-	string rhs, lhs, op, LD("LD"), SW("SW");
+	string rhs, lhs, op;
 
 	while(i<j){
-		if(p[i]!='*' || p[i]!='+' || p[i]!= '-' || p[i]!= '/'){
-			s.push(p[i]);
-		}else{
+		if((p[i]=='*') || (p[i]=='+') || (p[i]== '-') || (p[i]== '/')){
+	   
 			rhs = s.pop();
 			lhs = s.pop();
 			if(p[i]=='*')
@@ -65,15 +64,49 @@ Stack<string> postfix2assembler(std::vector<string>& p){
 				op = "AD";
 			if(p[i]=='-')
 				op = "SB";
-
+		
+           if(lhs[0]!='T' && lhs[1]!='M' && lhs[2]!='P'){
+		
+			as.push("LD");
+			as.push(lhs);
+			as.push(op);
+			as.push(rhs);
+			as.push("ST");
+			as.push("TMP");
+			s.push("TMP");
+			
+		   }else{
+			   as.push("LD");
+			   as.push(rhs);
+			   as.push(op);
+			   as.push(lhs);
+			   as.push("ST");
+			   as.push("TMP");
+			   s.push("TMP");
+		   }
+			
+		}else{
+		    s.push(p[i]);
 			}
 		++i;
+
+	}
+	
+	while(!as.IsEmpty()){
+		string operand, op2;
+		operand = as.pop();
+		op2 = as.pop();
+		a.push(op2);
+		a.push(operand);
 	}
 	return a;
 }
 
 void outputassembler(std::ostream& out, Stack<string>& assembler){
 	
+	   while(!assembler.IsEmpty()){
+		out <<"    " << assembler.pop() << "    " << assembler.pop() << std::endl; 
+	}
 }
 
 
